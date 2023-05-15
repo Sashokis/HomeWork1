f883b93b6df749be58cc25122b8ca3abd1b5e838
sashokis <you@example.com>
2023-05-15 20:33:45 +0300

    3 task

--- hw2.js
+++ hw2.js
@@ -7,3 +7,7 @@ alert(a);
 // задание 2
 let year = 2007;
 alert(`${year} год - это год выпуска первого iPhone.`); 
+
+// задание 3
+let  nameJS = 'Брендан Эйх'; 
+alert(`${nameJS} является создателем языка JavaScript`);
