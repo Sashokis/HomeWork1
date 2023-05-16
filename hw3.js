60f3f4e22ea98a35643028623857fdbd2c7e1f94
sashokis <you@example.com>
2023-05-16 18:32:45 +0300

    5 task

--- hw3.js
+++ hw3.js
@@ -26,3 +26,47 @@ e = Number(b);
 b = Number(a);
 a = e;
 alert (a+b);
+
+//5
+let monthNomber = Number(prompt('Введите номер месяца'));
+switch (monthNomber) {
+    case 1:
+        alert('зима');
+        break;
+    case 2:
+        alert('зима');
+        break;
+    case 3:
+        alert('весна');
+        break;    
+    case 4:
+        alert('весна');
+        break;
+    case 5:
+        alert('весна');
+        break; 
+    case 6:
+        alert('лето');
+        break;      
+    case 7:
+        alert('лето');
+        break;    
+    case 8:
+        alert('лето');
+        break;  
+    case 9:
+        alert('осень');
+        break;
+    case 10:
+        alert('осень');
+        break;
+    case 11:
+        alert('осень');
+        break;
+    case 12:
+        alert('зима');
+        break;              
+    default:
+        alert('Упс! Таког месяца нет')
+        break;
+}
