b76ff1918363157b97718e6d7252258a03c2a8aa
sashokis <you@example.com>
2023-05-16 18:26:43 +0300

    1 task

---
+++ hw3.js
@@ -0,0 +1,11 @@
+// ДОМАШКА 3
+ 
+// 1
+let password = 'пароль';
+let check = String(prompt('Введите пароль'));
+
+if (password === check ){
+    alert('Пароль введен верно');
+} else{
+    alert('Пароль введен неверно');
+}
