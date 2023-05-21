f8a4ab8720f6687f66cd3582c4c868195d5317ca
sashokis <you@example.com>
2023-05-21 17:39:59 +0300

    4 task

--- hw4.js
+++ hw4.js
@@ -1,14 +1,25 @@
-//1
-for (i = 1; i < 3; i++){
-    console.log('Привет!');
-}
+// //1
+// for (i = 1; i < 3; i++){
+//     console.log('Привет!');
+// }
 
-//2
-for (i = 1; i <= 5; i++){
-    console.log(i);
-}
+// //2
+// for (i = 1; i <= 5; i++){
+//     console.log(i);
+// }
 
-//3
-for (i = 7; i < 23; i++){
-    console.log(i);
+// //3
+// for (i = 7; i < 23; i++){
+//     console.log(i);
+// }
+
+//4
+const obj ={
+    'Коля': 200,
+    'Вася': 300, 
+    'Петя': 400
+};
+
+for (let name in obj) {
+    alert(`${name} - зарплата ${obj[name]} долларов.`);
 }
