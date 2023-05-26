442bd768a85e699407d4457e5c5e02b7f7ce7cb7
sashokis <you@example.com>
2023-05-26 10:24:50 +0300

    1 task

---
+++ hw5.js
@@ -0,0 +1,11 @@
+// 1
+let answer =0;
+function compare(one, two) {
+    if (one <= two) {
+        return answer = one;
+    } else { 
+        return answer = two;
+    }
+}
+
+alert(compare(6,6));
