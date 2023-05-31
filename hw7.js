1160eff394e5e067a1f25b4cea70a1b2801ef459
sashokis <you@example.com>
2023-05-31 22:22:17 +0300

    2 task

--- hw7.js
+++ hw7.js
@@ -2,3 +2,19 @@
 let stringUp = 'Hello!';
 stringUp = stringUp.toUpperCase();
 console.log(stringUp);
+
+// 2
+arrString = ['Кошка', 'Кит', 'Комар', 'Носорог'];
+nameString = 'ко'
+
+function searchStart (arr, nameSt){
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].toUpperCase().includes(nameString.toUpperCase())){
+            newArr.push(arr[i]);
+        }            
+    }
+    console.log(newArr);
+}
+
+searchStart(arrString, nameString);
