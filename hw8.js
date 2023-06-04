2761d84def506d3a6f9e95e78608db450beaaad3
sashokis <you@example.com>
2023-06-05 00:17:49 +0300

    1 task

---
+++ hw8.js
@@ -0,0 +1,20 @@
+// 1
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ 
+ console.log(
+    people.sort(function(a,b)
+        {
+            if (a.age >= b.age){
+                return 1;
+            }
+            if (a.age < b.age){
+                return -1;
+            }            
+        }
+        )
+    );
