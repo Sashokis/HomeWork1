e364d0d7a9c3de87cff2ed799363afd7c814114e
sashokis <you@example.com>
2023-06-05 13:05:16 +0300

    2 task

--- hw8.js
+++ hw8.js
@@ -18,3 +18,36 @@ const people = [
         }
         )
     );
+
+
+// 2
+function isPositive(number) {
+    if (number >= 0) {
+        return true;
+    } 
+}
+function isMale(namePerson) {
+    if (namePerson.gender === 'male') {
+        return true;
+    } 
+}
+function filter(arr, ruleFunction) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i]) === true) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive)); 
+
+const people = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people, isMale)); 
